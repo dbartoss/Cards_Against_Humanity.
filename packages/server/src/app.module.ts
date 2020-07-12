f5fd82843389e5ecf3156a6de17a7ba010b65827
dbartoss <dawid.bartosz@miquido.com>
2020-07-12 13:00:30 +0200

    build(database): Add additional params to mongoose setup

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -6,7 +6,11 @@ import { RoomsModule } from './rooms/rooms.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(`mongodb://localhost/${process.env.DB_NAME}`),
+    MongooseModule.forRoot(`mongodb://localhost/${process.env.DB_NAME}`, {
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    }),
     AuthModule,
     RoomsModule,
   ],
