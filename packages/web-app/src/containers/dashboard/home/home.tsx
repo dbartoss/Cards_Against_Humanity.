e0b900a709987e5c08b4e655339a82de03f4eeac
dbartoss <dawid.bartosz@miquido.com>
2020-11-04 19:54:34 +0000

    feat(dashboard-home): Add proper links styling and solve issue with invalidate DOM nesting of the hyperlink tags

--- packages/web-app/src/containers/dashboard/home/home.tsx
+++ packages/web-app/src/containers/dashboard/home/home.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { Paper, makeStyles, createStyles, Theme, Zoom, Link, Typography } from '@material-ui/core';
+import { Paper, makeStyles, createStyles, Theme, Zoom, Typography, Link as LinkComponent } from '@material-ui/core';
+import { RoomModel } from '../../../models/room.model';
 import { fetchRooms } from '../../../store/middlewares/rooms.thunks';
-import { openRoomsSelector } from '../../../store/selectors/rooms.selectors';
+import { currentRoom, openRoomsSelector } from '../../../store/selectors/rooms.selectors';
+import { userIdSelector } from '../../../store/selectors/user.selectors';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -12,12 +15,36 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         strong: {
             fontWeight: 500,
+        },
+        container: {
+            display: 'flex',
+            alignItems: 'center',
+        },
+        link: {
+            color: theme.palette.primary.main,
+            textDecoration: 'none',
+            '&:hover': {
+              textDecoration: 'underline',
+            },
+            '&:visited': {
+                color: theme.palette.primary.main,
+            },
+            '&:link': {
+                color: theme.palette.primary.main,
+            },
+            '&:active': {
+                color: theme.palette.primary.main,
+                textDecoration: 'underline',
+            },
         }
     }),
 );
 
 const Home: React.FC = (props) => {
-    const openRooms = useSelector(openRoomsSelector);
+    const userId: string | null = useSelector(userIdSelector);
+    const openRooms: RoomModel[]  = useSelector(openRoomsSelector);
+    const activeRoom: RoomModel | null = useSelector(currentRoom(userId));
+
     const dispatch = useDispatch();
     const classes = useStyles();
 
@@ -27,18 +54,32 @@ const Home: React.FC = (props) => {
 
     return (
         <React.Fragment>
-            <Zoom in={true} timeout={300} style={{ transitionDelay: '200ms' }}>
+            {activeRoom && (
+                <Zoom in={true} timeout={300} style={{ transitionDelay: '200ms' }}>
+                    <Paper className={classes.paper}>
+                        <Typography variant="subtitle1">
+                            You are currently in the room: <strong className={classes.strong}>{activeRoom?.name}</strong>
+                        </Typography>
+                        <Link to={`/dashboard/rooms/${activeRoom?.id}`}  className={classes.link}>
+                            Go to the room.
+                        </Link>
+                    </Paper>
+                </Zoom>
+            )}
+            <Zoom in={true} timeout={300} style={{ transitionDelay: '400ms' }}>
                 <Paper className={classes.paper}>
                     <Typography variant="subtitle1">
                         There now <strong className={classes.strong}>{openRooms.length}</strong> open rooms right now.
                     </Typography>
-                    <Link>See all rooms here.</Link>
+                    <Link to="/dashboard/rooms" className={classes.link}>See all rooms there.</Link>
                 </Paper>
             </Zoom>
-            <Zoom in={true} timeout={300} style={{ transitionDelay: '400ms' }}>
+            <Zoom in={true} timeout={300} style={{ transitionDelay: '600ms' }}>
                 <Paper className={classes.paper}>
-                    <Typography variant="subtitle1">Do you want to join to room?</Typography>
-                    <Link>Create a new room then.</Link>
+                    <Typography variant="subtitle1">Do you want to create a new to room?</Typography>
+                    <Link to="/dashboard/rooms/create"  className={classes.link}>
+                        Create a new room here.
+                    </Link>
                 </Paper>
             </Zoom>
         </React.Fragment>
