671d8722514a100a3f3858284771bf6083ece673
dbartoss <dawid.bartosz@miquido.com>
2020-11-04 03:06:47 +0000

    feat(auth-actions): Add missing actions for success and error statuses

--- packages/web-app/src/store/actions/auth.actions.ts
+++ packages/web-app/src/store/actions/auth.actions.ts
@@ -1,6 +1,6 @@
 import { useMemo } from 'react';
 import { Dispatch, bindActionCreators } from 'redux';
-import { Action, ActionFunctionAny, createAction } from 'redux-actions';
+import { createAction } from 'redux-actions';
 
 export enum Type {
     SIGN_IN = 'SIGN_IN',
@@ -15,13 +15,29 @@ export enum Type {
 }
 
 export const signInUser = createAction(Type.SIGN_IN);
+export const signInUserSuccess = createAction(Type.SIGN_IN_SUCCESS);
+export const signInUserError = createAction(Type.SIGN_IN_ERROR);
+
 export const signUpUser = createAction(Type.SIGN_UP);
+export const signUpUserSuccess = createAction(Type.SIGN_UP_SUCCESS);
+export const signUpUserError = createAction(Type.SIGN_UP_ERROR);
+
 export const logoutUser = createAction(Type.LOGOUT);
+export const logoutUserSuccess = createAction(Type.LOGOUT_SUCCESS);
+export const logoutUserError = createAction(Type.LOGOUT_ERROR);
 
 const actions = {
     signInUser,
+    signInUserSuccess,
+    signInUserError,
+
     signUpUser,
+    signUpUserSuccess,
+    signUpUserError,
+
     logoutUser,
+    logoutUserSuccess,
+    logoutUserError,
 };
 
 export const useAuthActions = (dispatch: Dispatch): typeof actions => {
