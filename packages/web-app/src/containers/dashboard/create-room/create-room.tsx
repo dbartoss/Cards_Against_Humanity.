c4532de77b2d399da95f2d5ff9db4fc94c0fcd84
dbartoss <dawid.bartosz@miquido.com>
2020-11-04 20:50:45 +0000

    feat(dashboard-create-room): Add logic with creating new room and redirecting on success to specific room view

--- packages/web-app/src/containers/dashboard/create-room/create-room.tsx
+++ packages/web-app/src/containers/dashboard/create-room/create-room.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { Paper, Typography, Button, TextField } from '@material-ui/core';
-import { createRoom } from '../../../store/actions/rooms.actions';
+import { createNewRoom } from '../../../store/middlewares/rooms.thunks';
+import { userIdSelector } from '../../../store/selectors/user.selectors';
 
 const CreateRoom: React.FC = (props) => {
     const [roomName, setRoomName] = React.useState('');
+    const userId: string | null = useSelector(userIdSelector);
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const handleTextFieldChanges = (event: React.ChangeEvent<HTMLInputElement>) => setRoomName(event.target.value);
-    const handleButtonClick = () => dispatch(createRoom());
+    const handleButtonClick = async () => dispatch(createNewRoom(roomName, userId, history.push));
 
     return (
         <Paper>
--- packages/web-app/src/models/room.model.ts
+++ packages/web-app/src/models/room.model.ts
@@ -5,6 +5,26 @@ export interface RoomModel {
     name: string;
     players: PlayerModel[];
     createdAt?: string;
+    currentRound?: CurrentRound;
 }
 
 export type GetRoomsApiResponse = RoomModel[];
+
+export interface NewRoomDTO {
+    name: string;
+    userId: string;
+}
+
+export interface CurrentRound {
+    _id: string;
+    selector: string | null;
+    mainCardText: string | null;
+    mainCardGaps: number | null;
+    currentlySelectedCards: CurrentlySelectedCard[];
+}
+
+export interface CurrentlySelectedCard {
+    _id: string;
+    userId: string;
+    cardText: string;
+}
